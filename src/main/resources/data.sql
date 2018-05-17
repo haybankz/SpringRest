f813676eb71e10e5c5a9a54fe95a1e98681a0519
haybankz <sunsyde2010@gmail.com>
2018-05-17 11:05:06 +0100

    included h2 db and prefined database data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+-- pre defined data
+INSERT INTO BLOG (title, content) VALUES( 'Hello', 'Hello world');
+
+INSERT INTO BLOG (title, content) VALUES( 'Hello 2', 'Hello world 2');
+
+INSERT INTO BLOG (title, content) VALUES( 'Hello 3', 'Hello world 3');
+
+INSERT INTO BLOG (title, content) VALUES( 'Hello 4', 'Hello world 4');
